63a65a29cebcd245fd00aecbd89a2f091cf92760
Sarah Arnold <digipet007@gmail.com>
2019-09-01 19:44:07 -0700

    working on toggling data attribute to animated for newly-renered images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,6 +23,7 @@ function populateButtons(topics, classToAdd, areaToAdd){
 //on-click function will grab user input and complete the
 //AJAX call
 $(document).on("click", ".searchButton", function(){
+    $("#populate-gifs-here").empty();
     var dataType = $(this).data("type");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + dataType + "&api_key=VikWXpQW2mBa4Wu7t4dEKeRdwDaGl9x9&limit=10";
     $.ajax({
@@ -32,17 +33,17 @@ $(document).on("click", ".searchButton", function(){
         // also create variables to store particular indexes of the response variable
     }).then(function(response){
         console.log(response);
-        for(let i = 0; i< response.data.length; i++){
+        for(let j = 0; j< response.data.length; j++){
             var searchDiv = $("<div class='gif-item'>");
-            var rating = response.data[i].rating;
+            var rating = response.data[j].rating;
             var p = $("<p>").text("Rating: " + rating);
             //variables to specify both animated versions of gifs and still versions of gifs, located within the response variable
-            var animated = response.data[i].images.fixed_height.url;
-            var still = response.data[i].images.fixed_height_still.url;
+            var animated = response.data[j].images.fixed_height.url;
+            var still = response.data[j].images.fixed_height_still.url;
             var image = $("<img>");
             //all images load to the page as still images first.
             image.attr("src", still);
-            //data-still and data-animated attributes confirm for us the image/gif reference
+            //changes the data attribute from data-still to  data-animated 
             image.attr("data-still", still);
             image.attr("data-animated", animated);
             //data-state attribute confirms for us the state of the gif
@@ -55,32 +56,35 @@ $(document).on("click", ".searchButton", function(){
         //need to use the document object because we are altering the already altered DOM
         $(document).on("click", ".searchImage", function(){
             //creates variable to store data-state of clicked image with class of searchImage
-            var state = $(this).data("state");
+            //cannot be .data("state"), as that only specifies the initial data state; the gif will not be able to change to its animated data-state attribute, and thus the else statement will never fire and the gif will be in constant motion.
+            var state = $(this).attr("data-state");
             //if the state is still, pull from the src for still image, stored in the animated variable. Else, pull from the still image source variable
-            if(state == still){
+            // console.log(state);
+            if(state === "still"){
                 $(this).attr("src", $(this).data("animated"));
                 $(this).attr("data-state", "animated");
             } else{
                 $(this).attr("src", $(this).data("still"));
                 $(this).attr("data-state", "still");
             }
-        })
-        //add new buttons to #buttons-go-here in a separate on-click function
-        $("#searchBtn").on("click", function(){
-            //prevent page from reloading
-            event.preventDefault();
-            $("#populate-gifs-here").empty();
-            //grab whatever is entered into the search bar
-            //eq grabs the first version of the input, since this element already has an input type and value
-            var newSearch = $("input").eq(0).val();
-            //populate the array with the new search term
-            topics.push(newSearch);
-            populateButtons(topics, "searchButton", "#buttons-go-here");
-            //input type of submit will reload the page, taking away the added buttons
-            //returning false will prevent this from happening
-            return false;
-        })
+        }) 
     })
+});
 
-})
-
+//add new buttons to #buttons-go-here in a separate on-click function
+$("#searchBtn").on("click", function(){
+    //prevent page from reloading. This is needed due to the button's bubmit type attribute.
+    event.preventDefault();
+    console.log("hello");
+    //grab whatever is entered into the search bar
+    //eq grabs the first version of the input, since this element already has an input type and value
+    var newSearch = $("input").eq(0).val();
+    
+    //populate the array with the new search term
+    topics.push(newSearch);
+    console.log(topics);
+    populateButtons(topics, "searchButton", "#buttons-go-here");
+    //input type of submit will reload the page, taking away the added buttons
+    //returning false will prevent this from happening
+    return false;
+});
